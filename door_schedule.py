b697b9fda3ef40e86dad04415b13748c36ab88f5
zhihang <35970192+zhihangfu@users.noreply.github.com>
2024-03-27 16:59:23 +0800

    Update door_schedule.py
    update to version 0.1

--- door_schedule.py
+++ door_schedule.py
@@ -1,7 +1,14 @@
 """
 read from the active autocad document "门窗编号“ block instances
 and create a door schedule from their attributes
+the schedule data should already exist as autocad block attributes
 """
+
+__author__ = "Zhihang Fu"
+__license__ = "MIT"
+__version__ = 0.1
+__email__ = "fuzhihang6829@gmail.com"
+
 
 import win32com.client
 import csv
@@ -9,39 +16,43 @@ import os
 
 
 acad = win32com.client.Dispatch("AutoCAD.Application")
-blockName = "门窗编号"  # block instance name to search for
-searchAtts = ["门窗主编号1", "门窗主编号2", "门窗辅编号1", "门窗辅编号2"]  # attributes to decide if a block is unique
 
+# block instance name to search for
+blockName = "门窗编号"
+# attributes to search for and decide a block's uniqueness
+searchAtts = ["门窗主编号1", "门窗主编号2", "门窗辅编号1", "门窗辅编号2"]
 
 schedule = {}
 
 
 def doorEntry(entity):
+	"""
+	convert door acad entity into a string entry for the schedule
+	"""
 	if entity.HasAttributes:
-		attTable = {}
+		# extract attributes as a dictionary
+		attTable = {}  
 		for attrib in entity.GetAttributes():
 			attTable[attrib.TagString] = attrib.TextString
-		doorEntry = " ".join([attTable[a] for a in searchAtts])
+		# create door entry as a string
+		doorEntry = "|".join([attTable[a] for a in searchAtts])
 		return doorEntry
 	else:
 		return None
 
 
 def addToSchedule(schedule, entity):
-	exist = False
 	entry = doorEntry(entity)
-	
+
 	if entry in schedule.keys():
 		schedule[entry] += 1
-		exist = True
-	if not exist:
+	else:
 		schedule[entry] = 1
 
 
 # iterate through all objects (entities) in the currently opened drawing
 for entity in acad.ActiveDocument.ModelSpace:
-	name = entity.EntityName
-	if name == "AcDbBlockReference":
+	if entity.EntityName == "AcDbBlockReference":
 		if entity.Name == blockName:
 			addToSchedule(schedule, entity)
 
@@ -52,8 +63,9 @@ filePath = acad.ActiveDocument.path
 path = os.path.join(filePath, fileName)
 with open(path, 'w', newline='') as f:
 	w = csv.writer(f)
-	w.writerow(["门窗编号","数量"])
-	w.writerows(schedule.items())
+	w.writerow(searchAtts + ["数量"])
+	for key in schedule.keys():
+		w.writerow(key.split("|") + [schedule[key]])
 
 # open the file just saved
 os.startfile(path)
